be1de7ef762a22dcc8428c76637463c0675bf13b
Yousef Mahmoud <usef.mahmoud@outlook.com>
2026-02-07 13:33:19 +0200

    feat: initialize my-react-query project with Vite, React, and TypeScript

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "my-react-query": path.resolve(__dirname, "./my-reacy-query/react-query"),
+    },
+  },
+});
